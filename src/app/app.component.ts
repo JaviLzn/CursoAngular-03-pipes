916c4b4d0d6ba56e801a8d27ec2976739d51c9a6
Javier Lozano <javier_l_o@hotmail.com>
2018-08-12 22:47:46 -0500

    Se corrije pipe currency y se crea pipe personalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- nombre = 'Fernando';
+  nombre = 'Fernando';
+  nombre2 = 'ferNAndo loPEZ';
+
  arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
  PI = Math.PI;
