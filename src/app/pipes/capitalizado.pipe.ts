20eefb1381af68fcd7fd937c6a4c8cfef9fe5130
Javier Lozano Ortiz <JavierL@DigitalWare.com.co>
2018-08-13 09:14:55 -0500

    se complementa pipe personalizado

--- src/app/pipes/capitalizado.pipe.ts
+++ src/app/pipes/capitalizado.pipe.ts
@@ -2,15 +2,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'capitalizado'})
 export class CapitalizadoPipe implements PipeTransform {
-    transform(value: string): string  {
+    transform(value: string, todas: boolean = true): string  {
 
         value = value.toLowerCase();
 
         const palabras = value.split(' ');
 
-        for (let i in palabras) {
+        if (todas) {
+         for (let i in palabras) {
             palabras[i] = palabras[i][0].toUpperCase() + palabras[i].substr(1);
+        }   
+        } else {
+            palabras[0] = palabras[0][0].toUpperCase() + palabras[0].substr(1);
         }
+        
 
         return palabras.join (' ');
     }
